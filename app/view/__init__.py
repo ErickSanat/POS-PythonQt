d0a172964cb5ec451649fccbb57e1bb22434759b
Erick C <erick12art@gmail.com>
2025-10-10 22:36:22 -0600

    update: actualizacion de nombres para arrastrar al main

--- app/view/__init__.py
+++ app/view/__init__.py
@@ -1,4 +1,5 @@
-from .generated.ui_login import Ui_InicioSesion
+from .generated.inicioSesionview_ui import Ui_InicioSesion
 from .generated.uiPagInicial import Ui_POS
 from .window.pagIni import TestWindow
 from .window.pagInicioSesion import InicioSesion
+
