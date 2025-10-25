f38561fb8a6f85f9ac01cffb34398926575b9fa4
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2025-10-25 01:56:35 -0600

    update: se actualizo el model en base a los cambios en DB

--- app/model/pago.py
+++ app/model/pago.py
@@ -1,26 +1,22 @@
-from .venta import Venta
 from .tipoPago import TipoPago
 
 class Pago:
     def __init__(
         self,
         id_pago: int=None,
-        venta: Venta=None,
         tipo_pago: TipoPago=None,
-        monto: float=None
+        cantidad: float=None
     ):
         self.id_pago = id_pago
-        self.venta = venta
         self.tipo_pago = tipo_pago
-        self.monto = monto
+        self.cantidad = cantidad
     
     def __repr__(self):
         from json import dumps, loads
         return dumps(
             {
                 "id_pago": self.id_pago,
-                "venta": loads(repr(self.venta)),
                 "tipo_pago": loads(repr(self.tipo_pago)),
-                "monto": float(self.monto)
+                "cantidad": float(self.cantidad)
             }, indent=4
         )
--- app/model/venta.py
+++ app/model/venta.py
@@ -1,19 +1,25 @@
-from .usuario import Usuario
+from .empleado import Empleado
 from .cliente import Cliente
+from .pago import Pago
+from .promocion import Promocion
 from datetime import datetime
 class Venta:
     def __init__(
         self,
         id_venta: int=None,
         fecha: datetime=None,
-        usuario: Usuario=None,
+        empleado: Empleado=None,
         cliente: Cliente=None,
+        promocion: Promocion=None,
+        pago: Pago=None,
         total: float=None,
     ):
         self.id_venta  = id_venta 
         self.fecha = fecha
-        self.usuario  = usuario 
+        self.empleado  = empleado 
         self.cliente = cliente
+        self.promocion = promocion
+        self.pago = pago
         self.total = total
     
     def __repr__(self):
@@ -21,9 +27,11 @@ class Venta:
         return dumps(
             {
                 "id_venta": self.id_venta,
-                "fecha": self.fecha.isoformat(),
-                "usuario": loads(repr(self.usuario)),
+                "fecha": self.fecha,
+                "empleado": loads(repr(self.empleado)),
                 "cliente": loads(repr(self.cliente)),
+                "promocion": loads(repr(self.promocion)),
+                "pago": loads(repr(self.pago)),
                 "total": float(self.total),
             }, indent=4
         )
